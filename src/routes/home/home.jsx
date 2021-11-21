50758c13d5345bf81c620988dd7bf3d8624f5b71
kylemcloughlin <kylemcloughlin1000@hotmail.ca>
2021-11-21 17:45:20 -0500

    fixxed number

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
         <div className='quest'>
 
           <h4 className='quest-one'><b>LANDSCAPING QUESTIONS?</b></h4>
-          <h3><b>(705) 931-4734</b></h3>
+          <h3><b>(705) 868-3560</b></h3>
           <p>Talk to a professional.</p>
         </div>
         <div>
